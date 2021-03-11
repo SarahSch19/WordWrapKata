3254143126495f4992cddf8254afc0b02c3d2e8b
Nigth <omnes.theo@gmail.com>
2021-03-11 23:42:30 +0100

    done

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
 
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
 
-        // TODO
+        ArrayList<Integer> spaces = new ArrayList<>();
+        int i;
+        int lastLF = 0;
 
+        for (i = 0; i < input.length(); i++)
+            if( input.charAt(i) == ' ' )  spaces.add(i);
+
+        spaces.add(input.length()-1);
+
+        for(i = 0; i < spaces.size()-1; i++ ) {
+            if( spaces.get(i+1) > columnNumber + lastLF) {
+                input = changeCharInPosition(spaces.get(i), '\n', input);
+                lastLF = spaces.get(i) + 1 ;
+            }
+        }
         return input;
+    }
+
+    public static String changeCharInPosition(int position, char ch, String str){
+        return str.substring(0,position) + ch + str.substring(position+1);
     }
 }
